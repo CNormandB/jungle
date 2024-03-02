2e9f793f45e0707ba7f2f8ce5d716b653db27e50
Casandra Normand <casandra.normand@outlook.com>
2024-03-02 06:51:53 -0500

    update categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,9 +9,8 @@ class Admin::CategoriesController < ApplicationController
       end
     end
   
-    private
-  
     def category_params
       params.require(:category).permit(:name)
     end
   end
+end
