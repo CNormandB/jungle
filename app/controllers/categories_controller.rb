677256f3f1aed9235570d6edb6f69fba2caaa87f
Casandra Normand <casandra.normand@outlook.com>
2024-03-02 06:08:12 -0500

    update controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,18 @@
 class CategoriesController < ApplicationController
 
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+  def index
+    @categories = Category.all
   end
 
-end
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
